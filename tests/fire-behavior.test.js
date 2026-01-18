8880653a12d788d75fde1e847f25cc157d044443
jamesdcochran-oss <jamesdcochran@gmail.com>
2026-01-18 18:25:26 -0500

    Initialize sample test for fire-behavior.js

---
+++ tests/fire-behavior.test.js
@@ -0,0 +1,7 @@
+// Sample test for fire-behavior.js
+
+describe('Fire Behavior Tests', () => {
+    test('should calculate fire rate correctly', () => {
+        // Add test logic here
+    });
+});
